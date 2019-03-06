90976f6beada32eea467e2521634e2805697d7c7
Kenton Lam <kenton_lam@outlook.com>
2019-03-06 22:40:43 +1000

    Convert to sympy.

--- simplex.py
+++ simplex.py
@@ -1,4 +1,4 @@
-import numpy as np
+import sympy as sp
 from collections import namedtuple
 
 Solution = namedtuple('Solution', 'variables objective')
@@ -11,32 +11,33 @@ def iterate(basic, non_basic, c, A, b):
     print('basis', basic)
 
     # matrix of columns of A in the basis
-    B = np.column_stack(A[:,b] for b in basic)
-    B_inv = np.linalg.inv(B)
+    print()
+    B = A[:, list(basic)]
+    B_inv = B**-1
     print('B matrix:')
     print(B)
 
     # basic feasible solution
-    x_B = B_inv @ b
+    x_B = B_inv * b
     print('x_B vector', x_B)
 
     # cost coefficients of variables in the basis
-    c_B = np.array([c[b] for b in basic])
+    c_B = sp.Matrix([c[b] for b in basic])
     print('c_B', c_B)
 
     # objective function value
-    z_B = np.dot(c_B, x_B)
+    z_B = c_B.T * x_B
     print('z_B', z_B)
 
     # compute dual variables
-    y = (B_inv).T @ c_B
+    y = (B_inv).T * c_B
     print('y', y)
 
     # iterate over non-basic variables to select one to enter
     feasible_entering_vars = []
     for j in non_basic:
         # compute reduced cost
-        c_j_prime = c[j] - y.T @ A[:,j]
+        c_j_prime = c[j] - (y.T * A[:,j])[0, 0]
 
         if c_j_prime > 0:
             feasible_entering_vars.append((c_j_prime, j))
@@ -50,7 +51,7 @@ def iterate(basic, non_basic, c, A, b):
     p_j = A[:,j]
 
     # determine leaving variable by element-wise dividing x_B and alpha_j.
-    alpha_j = B_inv @ p_j
+    alpha_j = B_inv * p_j
     ratios = [(x_B[k] / alpha_j[k], basic[k]) for k in range(len(x_B)) if alpha_j[k] > 0]
     print('ratio test', ratios)
 
@@ -80,6 +81,12 @@ def optimise(c: np.array, A: np.ndarray, b: np.array, num_vars) -> np.array:
     # number of decision variables
     n = num_vars
 
+    c = sp.Matrix(c)
+    A = sp.Matrix(A)
+    b = sp.Matrix(b)
+    augmented = A.col_insert(A.shape[1], b)
+    augmented.rref()
+
     # starting basic variables as slack/surplus variables
     basic = list(range(n, A.shape[1]))
     non_basic = list(range(n))
@@ -94,7 +101,6 @@ GE = object()
 class Simplex:
     def __init__(self):
         self.num_vars = 0
-        self.A = []
         self.constraints = []
         self.obj_coeff = []
 
@@ -134,8 +140,6 @@ class Simplex:
             A.append(row)
             b.append(constr[2])
 
-        A = np.array(A)
-        print(A)
         return optimise(c, A, b, self.num_vars)
 
 
@@ -144,8 +148,9 @@ if __name__ == "__main__":
         .add_vars(2)
         .add_constraint([2, 1], LE, 100)
         .add_constraint([1, 1], LE, 80)
-        #.add_constraint([1, 0], LE, 40)
-        .add_constraint([1, 0], EQ, 1)
+        .add_constraint([1, 0], LE, 40)
+        .add_constraint([1, 0], GE, 40)
+        # .add_constraint([1, 2], EQ, 25)
         .set_objective([3, 2])
         .optimise()
     )
