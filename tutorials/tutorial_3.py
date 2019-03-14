1214bb68aa82b5f2190e2c9140472bdbf9ae8be0
Kenton Lam <kenton_lam@outlook.com>
2019-03-14 18:03:46 +1000

    Use addVars.

--- tutorials/tutorial_3.py
+++ tutorials/tutorial_3.py
@@ -1,19 +1,4 @@
 from gurobipy import * 
-import numpy as np 
-
-def var_matrix(model: Model, rows, cols, prefix=''):
-    variables = []
-    for r in rows:
-        variables.append([])
-        for c in cols:
-            variables[-1].append(model.addVar(name=f'{prefix}{r}{c}'))
-    return np.array(variables)
-
-def print_vars(text, variables):
-    print(text, end=' ')
-    for v in variables:
-        print(round(v.x), end=' ')
-    print()
 
 def blending_problem_2():
     model = Model('Blending Problem 2')
@@ -23,17 +8,17 @@ def blending_problem_2():
     NonVeg = [2, 3, 4]
     Time = range(6)
 
-    Hardnesses = np.array([8.8, 6.1, 2.0, 4.2, 5.0])
+    Hardnesses = [8.8, 6.1, 2.0, 4.2, 5.0]
 
-    Costs = np.array([
-        [110, 130, 110, 120, 100, 90],
-        [120, 130, 140, 110, 120, 100],
-        [130, 110, 130, 120, 150, 140],
-        [110, 90, 100, 120, 110, 80],
-        [115, 115, 95, 125, 105, 135]
-    ])
+    Costs = [
+        [-110, -130, -110, -120, -100, -90],
+        [-120, -130, -140, -110, -120, -100],
+        [-130, -110, -130, -120, -150, -140],
+        [-110, -90, -100, -120, -110, -80],
+        [-115, -115, -95, -125, -105, -135]
+    ]
 
-    Initial = np.array([500]*len(Oils))
+    Initial = [500]*len(Oils)
 
     HMin = 3
     HMax = 6
@@ -44,47 +29,43 @@ def blending_problem_2():
     StoreMax = 1000
     StoreCost = 5
 
-    X = var_matrix(model, Oils, Time, 'x_')
-    Y = var_matrix(model, Oils, Time, 'y_')
-    S = var_matrix(model, Oils, Time, 's_')
+    ProfitPerUnit = 150
+    StorageCost = 5
+
+    X = model.addVars(Oils, Time, obj=ProfitPerUnit, name='X')
+    Y = model.addVars(Oils, Time, obj=Costs, name='Y')
+    S = model.addVars(Oils, Time, obj=-StorageCost, name='S')
 
     # at each time step.
     for t in Time:
         # veg processing constraint
-        model.addConstr(quicksum(X[Veg,t]) <= VegMax)
+        model.addConstr(quicksum(X[v,t] for v in Veg) <= VegMax)
         # non-veg processing constraint
-        model.addConstr(quicksum(X[NonVeg,t]) <= NonVegMax)
+        model.addConstr(quicksum(X[nv,t] for nv in NonVeg) <= NonVegMax)
 
-        for i in Oils:
-            # storage constraint
-            model.addConstr(S[i,t] <= StoreMax)
+        # storage constraint
+        model.addConstrs(S[i,t] <= StoreMax for i in Oils)
 
-            # first month. use initial oil.
-            if t == 0:
-                model.addConstr(S[i,t] == 500 - X[i,t] + Y[i,t])
-            else:
-                # storage at end of this month is previous month's storage
-                # minus processing plus purchases this month.
-                model.addConstr(S[i,t] == S[i,t-1] - X[i,t] + Y[i,t])
+        # first month. use initial oil.
+        if t == 0:
+            model.addConstrs(S[i,t] == 500 - X[i,t] + Y[i,t] for i in Oils)
+        else:
+            # storage at end of this month is previous month's storage
+            # minus processing plus purchases this month.
+            model.addConstrs(S[i,t] == S[i,t-1] - X[i,t] + Y[i,t] for i in Oils)
 
-    OilOnes = np.ones(len(Oils))
-    TimeOnes = np.ones(len(Time))
-
-    # parts of the objective 
-    Income = quicksum(150*X[i,t] for i in Oils for t in Time)
-
-    BuyingCost = quicksum(Y[i,t]*Costs[i,t] for i in Oils for t in Time)
-
-    StorageCost = quicksum(5*S[i,t] for i in Oils for t in Time)
-
-    model.setObjective(Income - StorageCost - BuyingCost, GRB.MAXIMIZE)
+    model.modelSense = GRB.MAXIMIZE
     model.optimize()
+    
+    format_list = lambda items: ' '.join(f'{k}: {v.x}' for k, v in items if v.x)
 
     for t in Time:
         print('Month', t)
-        print_vars('X', X[:,t])
-        print_vars('Y', Y[:,t])
-        print_vars('S', S[:,t])
+        for d in (X, Y, S):
+            for i in Oils:
+                var = d[i,t]
+                print(var.x, end=' ')
+            print()
 
     pass 
 
