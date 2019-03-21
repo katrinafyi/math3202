0079534ca1516e8a9116a2cbad5bda95041c530b
Kenton Lam <kenton_lam@outlook.com>
2019-03-21 10:38:08 +1000

    Remove lists of each market type.

--- tutorials/tutorial_4.py
+++ tutorials/tutorial_4.py
@@ -1,7 +1,9 @@
 from gurobipy import * 
 
 def portfolio_optimisation():
-    # multiplier for investments in each product.
+    # objective coefficients for investments in each product.
+    # for example, short-term bonds return 3.6% so its objective coefficient
+    # is 1.036.
     Returns = {
         'Cars (Germany)': 10.3/100 + 1,
         'Cars (Japan)': 10.1/100 + 1,
@@ -16,16 +18,6 @@ def portfolio_optimisation():
     }
     Products = list(Returns.keys())
 
-    filter_products = lambda p: [prod for prod in Products if p in prod]
-
-    Cars = filter_products('Cars')
-    Computers = filter_products('Computers')
-    Appliances = filter_products('Appliances')
-    Insurance = filter_products('Insurance')
-    Bonds = filter_products('bonds')
-    Germany = filter_products('Germany')
-    USA = filter_products('USA')
-
     m = Model('Portfolio Optimisation')
     
     # dollars to invest into each product.
@@ -34,17 +26,18 @@ def portfolio_optimisation():
     # total amount to invest.
     m.addConstr(X.sum('*') == 100000)
 
-    investment_in = lambda products_list: quicksum(X[p] for p in products_list)
+    products_of = lambda p: (prod for prod in Products if p in prod)
+    investment_in = lambda product_type: quicksum(X[p] for p in products_of(product_type))
 
-    # investment requirements (1-9)
-    m.addConstr(investment_in(Cars) <= 30000)
-    m.addConstr(investment_in(Computers) <= 30000)
-    m.addConstr(investment_in(Appliances) <= 20000)
-    m.addConstr(investment_in(Insurance) >= 20000)
-    m.addConstr(investment_in(Bonds) >= 25000)
+    # investment requirements (1-8)
+    m.addConstr(investment_in('Cars') <= 30000)
+    m.addConstr(investment_in('Computers') <= 30000)
+    m.addConstr(investment_in('Appliances') <= 20000)
+    m.addConstr(investment_in('Insurance') >= 20000)
+    m.addConstr(investment_in('bonds') >= 25000)
     m.addConstr(X['Short-term bonds'] >= 0.4*X['Medium-term bonds'])
-    m.addConstr(investment_in(Germany) <= 50000)
-    m.addConstr(investment_in(USA) <= 40000)
+    m.addConstr(investment_in('Germany') <= 50000)
+    m.addConstr(investment_in('USA') <= 40000)
 
     m.modelSense = GRB.MAXIMIZE
     m.optimize()
