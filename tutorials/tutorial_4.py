ff36ab9c743b36ece033c56584a7f641e70bdd09
Kenton Lam <kenton_lam@outlook.com>
2019-03-21 10:32:08 +1000

    Tutorial 4 portfolio optimisation.

---
+++ tutorials/tutorial_4.py
@@ -0,0 +1,64 @@
+from gurobipy import * 
+
+def filter_products(product, all_products):
+    for p in all_products:
+        if product in p:
+            yield p
+
+def portfolio_optimisation():
+    # multiplier for investments in each product.
+    Returns = {
+        'Cars (Germany)': 10.3/100 + 1,
+        'Cars (Japan)': 10.1/100 + 1,
+        'Computers (USA)': 11.8/100 + 1,
+        'Computers (Singapore)': 11.4/100 + 1,
+        'Appliances (Europe)': 12.7/100 + 1,
+        'Appliances (Asia)': 12.2/100 + 1,
+        'Insurance (Germany)': 9.5/100 + 1,
+        'Insurance (USA)': 9.9/100 + 1,
+        'Short-term bonds': 3.6/100 + 1,
+        'Medium-term bonds': 4.2/100 + 1
+    }
+    Products = list(Returns.keys())
+
+    Cars = filter_products('Cars', Products)
+    Computers = filter_products('Computers', Products)
+    Appliances = filter_products('Appliances', Products)
+    Insurance = filter_products('Insurance', Products)
+    Bonds = filter_products('bonds', Products)
+    Germany = filter_products('Germany', Products)
+    USA = filter_products('USA', Products)
+
+    m = Model('Portfolio Optimisation')
+    
+    # dollars to invest into each product.
+    X = m.addVars(Products, obj=Returns)
+
+    # total amount to invest.
+    m.addConstr(X.sum('*') == 100000)
+
+    investment_in = lambda products_list: quicksum(X[p] for p in products_list)
+
+    # investment requirements (1-9)
+    m.addConstr(investment_in(Cars) <= 30000)
+    m.addConstr(investment_in(Computers) <= 30000)
+    m.addConstr(investment_in(Appliances) <= 20000)
+    m.addConstr(investment_in(Insurance) >= 20000)
+    m.addConstr(investment_in(Bonds) >= 25000)
+    m.addConstr(X['Short-term bonds'] >= 0.4*X['Medium-term bonds'])
+    m.addConstr(investment_in(Germany) <= 50000)
+    m.addConstr(investment_in(USA) <= 40000)
+
+    m.modelSense = GRB.MAXIMIZE
+    m.optimize()
+
+    print()
+    print('Objective:')
+    print(m.objVal)
+    print()
+    for name, variable in X.items():
+        name_padded = "{:<21}".format(name)
+        print(f'{name_padded}    {variable.x}')
+
+if __name__ == "__main__":
+    portfolio_optimisation()
