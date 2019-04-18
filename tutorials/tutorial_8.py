ebaa16e0d423a96e370705a2d46c43adf50bd58a
Kenton Lam <kenton_lam@outlook.com>
2019-04-18 11:47:16 +1000

    knapsack and vladimir problems.

--- tutorials/tutorial_8.py
+++ tutorials/tutorial_8.py
@@ -35,7 +35,46 @@ def _solve_study(hours: int, subject: int):
 def studying():
     return _solve_study(4, 0)
 
+def _vladimir(games: int, delta: int):
+    if games >= 2:
+        if delta > 0:
+            return (1, 'win')
+        elif delta < 0:
+            return (0, 'lose')
+        return (0.45, 'bold')
+
+    bold = (0.45*_vladimir(games+1, delta+1)[0] + 0.55*_vladimir(games+1, delta-1)[0], 'bold')
+    cons = (0.9*_vladimir(games+1, delta)[0] + 0.1*_vladimir(games+1, delta-1)[0], 'cons')
+    return max(bold, cons)
+
+values = [25, 12, 8]
+sizes = [7, 4, 3]
+
+@lru_cache()
+def knapsack(item: int, space: int):
+    if space == 0 or item > 2:
+        return (0, ())
+
+    max_value = 0
+    max_items = ()
+    for n in range(space): # number of item 'i' to put in knapsack
+        if n*sizes[item] > space: break
+        
+        v, i = knapsack(item+1, space-n*sizes[item])
+        val = n*values[item] + v
+
+        if val > max_value:
+            max_value = val
+            max_items = (n, ) + i
+
+    return (max_value, max_items)
+
+    
+
 if __name__ == '__main__':
-    print(studying())
-    print(_solve_study.cache_info())
+    # print(knapsack(0, 20))
+    print(knapsack.cache_info())
+    print(_vladimir(0, 0))
+    # print(studying())
+    # print(_solve_study.cache_info())
     # print(fib(int(sys.argv[1])))
