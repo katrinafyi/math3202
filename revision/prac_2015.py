6c5d5fb9458d1c8e5c9cb74edc800aca4bac1636
Kenton Lam <kenton_lam@outlook.com>
2019-06-13 17:18:35 +1000

    2015 prac: seasonal hiring.

---
+++ revision/prac_2015.py
@@ -0,0 +1,23 @@
+from functools import lru_cache
+
+def optimal_hiring():
+    SEASONS = ['Summer', 'Autumn', 'Winter', 'Spring', 'Summer']
+    REQUIRED = [155, 120, 140, 100, 155]
+
+    @lru_cache(maxsize=None)
+    def V(t, n):
+        if t >= len(SEASONS):
+            return (0, ())
+        
+        return min(
+            (200*(s-n)**2 
+                + 2000*(s-REQUIRED[t]) 
+                + V(t+1, s)[0], 
+            (s, ) + V(t+1, s)[1])
+            for s in range(REQUIRED[t], 155+1)
+        )
+    
+    print(V(0, 155))
+
+if __name__ == "__main__":
+    optimal_hiring()
